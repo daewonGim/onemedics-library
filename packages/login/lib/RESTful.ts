eb4164e65643eec0f31703485f855b0d3b2b165a
daewonGim <dwkim@onemedics.net>
2020-12-28 22:52:18 +0900

    [Onemedics-Frameworks] 로그인 - Axios 구현(테스트 완료 , 코드 리팩토링 완료) - Apollo 구현완료 (테스트 예정,  코드 리팩토링 예정) - Jest  테스트 코드 추가 예정

--- packages/login/lib/RESTful.ts
+++ packages/login/lib/RESTful.ts
@@ -1,4 +1,20 @@
-import axios, { AxiosInstance, AxiosResponse } from "axios";
+import axios, {AxiosInstance, AxiosRequestConfig, AxiosResponse} from "axios";
+
+export type JoinFormData = {
+    username: string;
+    loginId: string;
+    phone: string;
+    password: string;
+    authNum: string;
+    authNumId: string;
+    marketingAgreement: boolean;
+};
+export type ToastProps = {
+    toastMessage: string;
+    isVisible?: boolean;
+    backgroundColor: string;
+    isChecked?: boolean;
+};
 
 class Client {
     private axios: AxiosInstance;
@@ -10,8 +26,9 @@ class Client {
         API_REQUEST_TIMEOUT: number,
         APP_CLIENT_ID?: string,
         APP_VERSION?: string,
-        showToast?: (toastProps: any) => void
+        showToast?: (toastProps: ToastProps) => void
     ) {
+        this.token = OAUTH_BASIC_KEY;
         this.axios = axios.create({
             baseURL: GW_API_BASE_URL,
             timeout: API_REQUEST_TIMEOUT,
@@ -22,9 +39,28 @@ class Client {
                 AppVersion: APP_VERSION
             }
         });
+        // 공통 오류 처리
+        this.axios.interceptors.response.use(
+            (response) => {
+                // 성공 로직에 별도 추가하지 않음
+                return response;
+            },
+            (error) => {
+                // 실패 로직에 500 에러 메시지 표시
+                if (error && error.response && error.response.status === 500) {
+                    // 오류 toast 표시
+                    showToast && showToast({
+                        backgroundColor: 'red',
+                        toastMessage: '오류가 발생 했습니다. 관리자에게 문의바랍니다.',
+                    });
+                    return Promise.reject(error);
+                }
+                return Promise.reject(error);
+            },
+        );
     }
 
-    updateAuthorizationToken(accessToken?: string) {
+    updateAuthorizationToken(accessToken?: string | object) {
         this.axios.defaults.headers.Authorization = accessToken
             ? {
                 toString() {
@@ -34,13 +70,13 @@ class Client {
             : this.token;
     }
 
-    get<T>(path: string, payload?: any) {
+    get<T>(path: string, payload?: AxiosRequestConfig) {
         return this.axios
             .get<T>(path, payload)
             .then((response: AxiosResponse) => response);
     }
 
-    post(path: string, payload: any) {
+    post(path: string, payload: FormData | JoinFormData | JSON | string) {
         const options = {
             headers: {
                 "Content-Type":
@@ -55,64 +91,54 @@ class Client {
             .then((response: AxiosResponse) => response);
     }
 
-    put(path: string, payload: any) {
+    put(path: string, payload?: AxiosRequestConfig) {
         return this.axios
             .put(path, payload)
             .then((result: AxiosResponse) => result);
     }
 
-    delete(path: string, payload?: any) {
+    delete(path: string, payload?: AxiosRequestConfig) {
         return this.axios
             .delete(path, payload)
             .then((result: AxiosResponse) => result);
     }
+
+    async serverHealthCheck(){
+        return await this.get("/dosoo/health/check");
+    };
+
+    async oauthLogin(params: FormData){
+        this.updateAuthorizationToken(this.token);
+        return await this.post("/oauth/token", params);
+    };
+
+    async getUserInfo(access_token: string){
+        this.updateAuthorizationToken(`Bearer ${access_token}`);
+        return await this.get("/dosoo/v1/member/me");
+    };
+
+    async updateAppDeviceToken(access_token: string,params: FormData){
+        this.updateAuthorizationToken(`Bearer ${access_token}`);
+        return await this.post("/dosoo/v1/device-token", params);
+    };
+
+    async getEmrInfo(accessToken: string | null | void, paymentId: string){
+        return await this.post(`/dosoo/v2/api/pay/detail/${paymentId}`, JSON);
+    };
+
+    async createUserInfo(params: JoinFormData){
+        return await this.post("/auth/v1/user/signup", params);
+    };
+
+    async confirmPassword( password: string){
+        return await this.post("/auth/v1/user/confirmPassword", password);
+    };
+
+    async updatePassword( params: string){
+        return await this.post("/auth/v1/user/updatePassword", params);
+    };
 }
 
 export default Client;
 
-export const serverHealthCheck = async (client: Client, path: string) => {
-    return await client.get("/dosoo/health/check");
-};
 
-export const oauthLogin = async (
-    client: Client,
-    OAUTH_BASIC_KEY: string,
-    params: FormData
-) => {
-    client.updateAuthorizationToken(OAUTH_BASIC_KEY);
-    return await client.post("/oauth/token", params);
-};
-
-export const getUserInfo = async (client: Client, access_token: string) => {
-    client.updateAuthorizationToken(`Bearer ${access_token}`);
-    return await client.get("/dosoo/v1/member/me");
-};
-
-export const updateAppDeviceToken = async (
-    client: Client,
-    access_token: string,
-    params: object
-) => {
-    client.updateAuthorizationToken(`Bearer ${access_token}`);
-    return await client.post("/dosoo/v1/device-token", params);
-};
-
-export const getEmrInfo = async (
-    client: Client,
-    accessToken: string | null | void,
-    paymentId: string
-) => {
-    return await client.post(`/dosoo/v2/api/pay/detail/${paymentId}`, {});
-};
-
-export const createUserInfo = async (client: Client, params: object) => {
-    return await client.post("/auth/v1/user/signup", params);
-};
-
-export const confirmPassword = async (client: Client, password: string) => {
-    return await client.post("/auth/v1/user/confirmPassword", password);
-};
-
-export const updatePassword = async (client: Client, params: object) => {
-    return await client.post("/auth/v1/user/updatePassword", params);
-};
