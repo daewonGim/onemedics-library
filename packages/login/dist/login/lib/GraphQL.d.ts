9ecd82443233da291edb4a6ecbbedea101e6d1fc
daewonGim <dwkim@onemedics.net>
2020-12-30 09:57:16 +0900

    [Onemedics-Frameworks] - Code-Refactoring

--- packages/login/dist/login/lib/GraphQL.d.ts
+++
@@ -1,4 +0,0 @@
-import { ApolloClient, NormalizedCacheObject } from "apollo-boost";
-import { Storage } from "../../common/lib";
-export declare const WebLoginClient: (DOSOO_API_BASE_URL: string, ACCESS_TOKEN__OF__STORAGE_ACCESS_INFO: string, storage: Storage) => ApolloClient<any>;
-export declare const AppLoginClient: (DOSOO_API_BASE_URL: string, ACCESS_TOKEN__OF__STORAGE_ACCESS_INFO: string, storage: Storage, validateTokenExp: () => Promise<boolean>, OAUTH_BASIC_KEY: string, APP_CLIENT_ID: string, APP_VERSION: string) => ApolloClient<NormalizedCacheObject>;
